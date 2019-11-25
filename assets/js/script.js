9798945f64eeec507167b8551615d25738c9f54e
caro76 <chery.caroline76@gmail.com>
2019-11-25 14:07:50 +0100

    exercice2javascript

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+
+function survol(){
+  document.getElementById('photo').src='photo2.jpg';
+}
+
+function cache(){
+  document.getElementById('photo').src='photo.jpg';
+}
+
+function affichage() {
+  var write = document.getElementById('write').value;
+  alert(write);
+}
+function affichage(){}
